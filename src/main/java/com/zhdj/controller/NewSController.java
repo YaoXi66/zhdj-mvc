90095dec69ee3d679596df396315d0193ff31ec3
zhangjinhui152 <2233274242@qq.com>
2022-05-22 10:39:20 +0800

    添加异常方法 如果觉得提交太频繁 后面可以变基 删掉这些提交
    Signed-off-by: zhangjinhui152 <2233274242@qq.com>

--- src/main/java/com/zhdj/controller/NewSController.java
+++ src/main/java/com/zhdj/controller/NewSController.java
@@ -25,39 +25,50 @@ public class NewSController {
     @Autowired
     private HttpServletResponse httpServletResponse;
 
+    public String GetNewsHanderError(int type ,int page1,int page2){
+        JSONObject res = new JSONObject();
+        try{
+            res.put("data",newsService.SelectDynamicList(type,page1,page2));
+            res.put("result",200);
+        }
+        catch (Exception e){
+            res.put("data","总之！你错了");
+            res.put("result",400);
+            return res.toJSONString();
+        }
+        return res.toJSONString();
+    }
+
     @GetMapping(value = "/news/affairs",produces = "text/plain;charset=utf-8")
     public String GetAffairs(@RequestParam("page1")int page1,@RequestParam("page2")int page2){
-        JSONObject res = new JSONObject();
-        res.put("data",newsService.SelectDynamicList(0,page1,page2));
-        res.put("result",200);
-        return res.toJSONString();
+        return GetNewsHanderError(0,page1,page2);
     }
     @GetMapping(value = "/news/dynamic",produces = "text/plain;charset=utf-8")
     public String GetDynamic(@RequestParam("page1")int page1,@RequestParam("page2")int page2){
-        JSONObject res = new JSONObject();
-        res.put("data",newsService.SelectDynamicList(1,page1,page2));
-        res.put("result",200);
-        return res.toJSONString();
+        return GetNewsHanderError(1,page1,page2);
     }
     @GetMapping(value = "/news/policy",produces = "text/plain;charset=utf-8")
     public String GetPolicy(@RequestParam("page1")int page1,@RequestParam("page2")int page2){
-        JSONObject res = new JSONObject();
-        res.put("data",newsService.SelectDynamicList(2,page1,page2));
-        res.put("result",200);
-        return res.toJSONString();
+        return GetNewsHanderError(2,page1,page2);
     }
     @GetMapping(value = "/news/pioneer",produces = "text/plain;charset=utf-8")
     public String GetPioneer(@RequestParam("page1")int page1,@RequestParam("page2")int page2){
-        JSONObject res = new JSONObject();
-        res.put("data",newsService.SelectDynamicList(3,page1,page2));
-        res.put("result",200);
-        return res.toJSONString();
+        return GetNewsHanderError(3,page1,page2);
     }
     @PostMapping(value = "/news/release",produces = "text/plain;charset=utf-8")
     public String PostRelease(@RequestParam("user_id") int user_id, @RequestParam("id") int id, @RequestParam("time") String time, @RequestParam("link") String link, @RequestParam("type") String type, @RequestParam("title") String title, @RequestParam("preview") String preview){
-        System.out.println(newsService.addDynamicList(user_id, id, time, link,  type, title, preview));
         JSONObject res = new JSONObject();
         JSONObject msg = new JSONObject();
+        try{
+            newsService.addDynamicList(user_id, id, time, link,  type, title, preview);
+        }
+        catch (Exception e){
+            msg.put("msg","ERROR");
+            res.put("data",msg);
+            res.put("result",400);
+            return res.toJSONString();
+        }
+
         msg.put("msg","OK");
         res.put("data",msg);
         res.put("result",200);
